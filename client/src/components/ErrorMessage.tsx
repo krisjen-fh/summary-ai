52eb4c654f84bc87d9c3722b9418dfd04bce788d
krisjen-fh <krisjenfraulein23@gmail.com>
2026-07-04 02:39:17 +0700

    update warning design on ErrorMessage.tsx & index.css

--- client/src/components/ErrorMessage.tsx
+++ client/src/components/ErrorMessage.tsx
@@ -1,15 +1,22 @@
-type Props = {
-    message: string;
-};
+interface Props{
+    message:string;
+}
 
-const ErrorMessage = ({ 
-    message 
-}: Props) => {
-    return (
-        <div className="error-box">
-            {message}
+const ErrorMessage = ({message}:Props)=>{
+
+    return(
+        <div className="error-card">
+            <span className="error-icon">
+                ⚠️
+            </span>
+            <div>
+                <h4>
+                    Terjadi Kesalahan
+                </h4>
+                <p>{message}</p>
+            </div>
         </div>
     );
-};
+}
 
 export default ErrorMessage;
